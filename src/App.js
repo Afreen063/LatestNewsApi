2fd7851dee34b408f12c368c30f48f7e0982c76d
Afreen063 <afreen0501@gmail.com>
2022-09-06 21:22:19 +0530

    latest news Api with the help of Axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios, * as others from 'axios';
+import { useState } from 'react';
 
 function App() {
+const [newsData, setNewsData]=useState([]);
+
+
+   const handleClick=()=>
+  {
+    axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=9904b4a740bf476383cc75a848f17e0d')
+    .then((response)=>
+    {
+      setNewsData(response.data.articles)
+      console.log(response.data.articles);
+      
+    });
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}> Fetch Latest News</button>
+    {
+      newsData.map((data)=>{
+        return (
+         <div className='container'>
+          <div className='row'>
+          <div className='col'>
+            <div className="card" style={{width: "15rem"}}>
+  <img className="card-img-top" src={data.urlToImage} alt="Card image cap" />
+  <div className="card-body">
+    <h5 className="card-title">{data.title}</h5>
+    <p className="card-text">{data.description}</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+          </div>
+            </div>
+          </div>
+        );
+      })
+    }
     </div>
   );
 }
